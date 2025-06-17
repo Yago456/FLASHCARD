218565634fa5d895e5e89d540c737feb996f2113
Yago456 <yago.bezerra.silva@escola.pr.gov.br>
2025-06-17 19:09:52 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,18 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Quem é o Rafael Biano?',
+    'O maior proggramador de todos'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Games',
+    'Melhor jogo do ano?',
+    'Roblox'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Escola',
+    'Qual a matéria mais difícil?',
+    'Artes(achou que eu ia falar programação né)'
 )
 
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
-)
